272bffdc0f976311d01c336259f7c590f9856237
gerhardJaeger <gerhard.jaeger@uni-tuebingen.de>
2023-03-30 12:04:53 +0200

    convert correpondence patterns to phylip files

---
+++ get_phy_files.py
@@ -0,0 +1,58 @@
+#%%
+import pandas as pd
+import numpy as np
+import os
+
+#%%
+
+def get_char_mtx(db):
+    d = pd.read_table("correspondences/"+db)
+    languages = d.columns[3:-2]
+
+    matrices = []
+
+    for c in d.index:
+        char = d.iloc[c,:]
+        valueTokens = char.values[3:-2].astype('unicode')
+        valueTypes = pd.unique(valueTokens)
+        valueTypes = pd.Series([s for s in valueTypes if s != 'Ø'])
+        cMtx = np.repeat(
+            np.transpose(
+                np.vstack([valueTokens == s for s in valueTypes], dtype=int)
+                ),
+            int(char.FREQUENCY),
+            axis=1,
+        )
+        cMtx[valueTokens == 'Ø',:] = -1
+        matrices.append(cMtx)
+    return pd.DataFrame(np.hstack(matrices), index=languages)
+#%%
+def cm_to_phy(charMtx):
+    characters = np.array(['0', '1', '-'])
+    pad = max([len(x) for x in charMtx.index])+5
+    phy = f'{charMtx.shape[0]} {charMtx.shape[1]}\n'
+    for l in charMtx.index:
+        phy += l.ljust(pad)
+        phy += ''.join(characters[charMtx.loc[l]])+'\n'
+    return phy
+#%%
+
+dbs = os.listdir("correspondences")
+
+
+# %%
+phy_pth = "correspondences_phylip"
+isExist = os.path.exists(phy_pth)
+
+if not isExist:
+    os.mkdir(phy_pth)
+
+# %%
+
+for db in dbs:
+    charMtx = get_char_mtx(db)
+    phy = cm_to_phy(charMtx)
+    fn = os.path.join(phy_pth, db.split('.')[0] + '.phy')
+    with open(fn, 'w') as f:
+        f.write(phy)
+# %%
