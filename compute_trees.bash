4419132b00c474121016e7b1418e2d32d49e951b
gerhardJaeger <gerhard.jaeger@uni-tuebingen.de>
2023-04-01 08:11:25 +0200

    renaming ml trees

--- compute_trees.bash
+++ compute_trees.bash
@@ -2,6 +2,7 @@ for f in `ls cognate_classes_phylip`
 do
     db=`basename $f .phy`
     raxml-ng --msa cognate_classes_phylip/$f --model BIN --prefix cognate_classes_mltree/$db.phy --redo
+    cp cognate_classes_mltree/$db.phy.raxml.bestTree cognate_classes_mltree/$db"_ml.tre"
 done
 
 
@@ -9,6 +10,7 @@ for f in `ls correspondences_phylip`
 do
     db=`basename $f .phy`
     raxml-ng --msa correspondences_phylip/$f --model BIN --prefix correspondences_mltree/$db.phy --redo
+    cp correspondences_mltree/$db.phy.raxml.bestTree correspondences_mltree/$db"_ml.tre"
 done
 
 
