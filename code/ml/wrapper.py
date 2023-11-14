826d693e1f7385443bbc1c44ab4031134af55c59
Luise Haeuser <luise.haeuser@web.de>
2023-11-14 16:22:35 +0100

    Remove unecessary code

--- code/ml/wrapper.py
+++ code/ml/wrapper.py
@@ -6,41 +6,6 @@ exe_path = "./bin/raxml-ng"
 def best_tree_path(prefix):
     return prefix + ".raxml.bestTree"
 
-
-def consensus_tree_path(prefix):
-    return prefix + ".raxml.consensusTreeMR"
-
-def alpha(prefix):
-    with open(prefix + ".raxml.log", "r") as logfile:
-        lines = logfile.readlines()
-    for line in lines:
-        if line.startswith("   Rate heterogeneity:"):
-            return float(line.split(",  ")[1].split(" ")[1])
-    return float('nan')
-
-def aic(prefix):
-    with open(prefix + ".raxml.log", "r") as logfile:
-        lines = logfile.readlines()
-    for line in lines:
-        if line.startswith("AIC"):
-            return float(line.split(" / ")[0].split(" ")[2])
-    return float('nan')
-
-def consense_tree(prefixes, prefix):
-    with open("trees.nw", "w+") as outfile:
-        for p in prefixes:
-            tree_path = best_tree_path(p)
-            if os.path.isfile(tree_path):
-                with open(tree_path, "r") as infile:
-                    outfile.write(infile.read())
-    command = exe_path
-    command += " --consense"
-    command += " --tree trees.nw "
-    command += " --prefix " + prefix
-    os.system(command)
-    os.remove("trees.nw")
-
-
 def run_inference(msa_path, model, prefix, args = ""):
     if exe_path == "":
         print("Please specify raxmlng.exe_path")
